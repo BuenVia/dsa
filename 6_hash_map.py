9974ee61230e4c5e4771ac17a60e0bde3ea414f0
Matthew Clifford <matthewclifford@hotmail.co.uk>
2025-07-31 12:08:26 +0200

    added in code for hash maps

---
+++ 6_hash_map.py
@@ -0,0 +1,36 @@
+class HashMap:
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    def _hash(self, key):
+        h = 0
+        for k in key:
+            h = (h + ord(k) * 23) % len(self.data_map)
+        return h
+    
+    def set_hash(self, key, value):
+        index = self._hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_hash(self, key):
+        index = self._hash(key)
+        if self.data_map[index] != None:
+            for i in range(len(self.data_map)):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i]
+        return None
+    
+
+hm = HashMap()
+
+hm.set_hash("Valencia", 10)
+hm.set_hash("Madrid", 1)
+hm.set_hash("Barcelona", 9)
+hm.set_hash("Malaga", 45)
+hm.set_hash("Gijon", 33)
+
+print(hm.get_hash("Madrid"))
+
+print(hm.data_map)
